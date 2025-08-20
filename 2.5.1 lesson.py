9d257b9727cf40d000993e4188837450901bf355
Gleb <gleb.tanchev@yandex.ru>
2025-08-21 00:38:39 +0300

    Добил задание 2.5.1

--- 2.5.1 lesson.py
+++ 2.5.1 lesson.py
@@ -1,3 +1,5 @@
 grams = 15635
 weight_in_kilograms = grams/1000
-print(int(weight_in_kilograms))
+final_kilos = int (weight_in_kilograms)
+final_answer = f"В {grams} граммах содержится {final_kilos} полных килограмм"
+print (final_answer)
